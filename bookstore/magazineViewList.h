7535fd1b80419ae6f70ebb2e93d0c83eadecbc06
imaammdev <imaamm.dev@gmail.com>
2014-07-04 12:48:24 +0700

    Revert f2fff26..e515c9c
    This rolls back to commit f2fff26141fd327490261f65956ecedea23ada57.

---
+++ bookstore/magazineViewList.h
@@ -0,0 +1,13 @@
+//
+//  magazineViewList.h
+//  bookstore
+//
+//  Created by Herman Tolle on 7/3/14.
+//  Copyright (c) 2014 Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface magazineViewList : UIViewController
+
+@end
