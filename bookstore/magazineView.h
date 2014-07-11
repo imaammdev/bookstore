3aabb8900e2b867295b8b7096971f99a12e27b71
imaammdev <imaamm.dev@gmail.com>
2014-07-11 15:01:28 +0700

    Revert da9c6b5..f8a4862
    This rolls back to commit da9c6b5bad022dccf49b2ac6e01b2afd86da595a.

--- bookstore/magazineView.h
+++ bookstore/magazineView.h
@@ -2,13 +2,12 @@
 //  magazineView.h
 //  bookstore
 //
-//  Created by Herman Tolle on 7/11/14.
+//  Created by Herman Tolle on 7/4/14.
 //  Copyright (c) 2014 Lab. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface magazineView : UIViewController <UICollectionViewDataSource, UICollectionViewDelegateFlowLayout>
-@property (weak, nonatomic) IBOutlet UICollectionView *collectionViewMag;
+@interface magazineView : UICollectionViewController
 
 @end
